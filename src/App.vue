20b7c7e62753e060f7d87e18e705dcec4e088df9
tang <467938742@qq.com>
2021-08-19 21:52:20 +0800

    'firstblog'

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-view/>
+    </div>
+
+  </div>
+</template>
+
+<style>
+  #app {
+    max-width: 960px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/father.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="father">
+    <h3>这里是父组件</h3>
+    <child>
+      <div class="tmpl">
+        <span>菜单1</span>
+        <span>菜单2</span>
+        <span>菜单3</span>
+        <span>菜单4</span>
+        <span>菜单5</span>
+        <span>菜单6</span>
+      </div>
+    </child>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "father"
+}
+</script>
+
+<style scoped>
+
+</style>
